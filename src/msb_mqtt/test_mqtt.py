7c88d8d7f171703dbcfede5443941d40c7d15d93
felix <github@felix.weiler.cc>
2023-01-24 14:07:32 +0100

    Add MQTTnode, a zmq-sub mqtt-pub class

---
+++ src/msb_mqtt/test_mqtt.py
@@ -0,0 +1,97 @@
+import unittest 
+import paho.mqtt.client as mqtt
+import time
+import ssl
+
+from MQTTnode import MQTTnode 
+
+class MQTTconnectionTester(unittest.TestCase):
+    def setUp(self):
+        self.client = mqtt.Client()
+        self.client.username_pw_set("felix", "albatrozz")
+        self.client.on_connect = self.on_connect
+        # self.client.tls_set(tls_version=ssl.PROTOCOL_TLS_CLIENT) 
+        self.client.connect("localhost", 1883)
+        self.override = {"mqtt_broker" : "localhost",
+                    "user" : "felix",
+                    "passwd" : "albatrozz",
+                    "qos" : 2,
+                    "mqtt_port" : 1883,
+                    "ssl" : False,
+                    "topics" : ["test", "over"]}
+
+        self.mqttnode = MQTTnode(self.override)
+
+
+    def tearDown(self):
+        self.client.disconnect()
+
+
+    def test_publish(self):
+        (result, mid) = self.mqttnode.publish("test/topic", "Hello, MQTT!")
+        self.assertEqual(result, mqtt.MQTT_ERR_SUCCESS, "Failed to publish message")
+
+
+    def test_subscribe(self):
+        topic = "topic1"
+        test_message = "1009.988"
+        self.client.subscribe(f"turbine/{topic}")
+        self.client.on_message = self.on_message
+        self.client.loop_start()
+        # self.client.publish(topic, message, qos=1)
+        self.mqttnode.publish(topic, test_message)
+        time.sleep(3)
+        self.client.loop_stop()
+
+        expected_return = f"test turbine/topic1={test_message} {time.time_ns()}"
+        expected_return = self.strip_nanoseconds(expected_return)
+        received_return = self.strip_nanoseconds(self.message)
+        self.assertEqual(expected_return, received_return, "Incorrect message received")
+
+
+    def strip_nanoseconds(self, text):
+        return text[:-11]
+
+
+    def on_message(self, client, userdata, msg):
+        # print("received message")
+        self.message = msg.payload.decode()
+
+
+    def on_connect(self, lient, userdata, flags, rc):
+        if rc == 0:
+            # print('Test client connected to MQTT broker')
+            self.client.is_connected = True
+        else:
+            self.fail("Could not connect")
+
+
+class ConfigTester(unittest.TestCase):
+    def test_reads_override_correctly(self):
+        override = {"mqtt_broker" : "localhost",
+                    "user" : "felix",
+                    "passwd" : "albatrozz",
+                    "qos" : 2,
+                    "mqtt_port" : 1883,
+                    "ssl" : False,
+                    "topics" : ["test", "over"],
+                }
+        self.mqttnode = MQTTnode(override)
+        self.assertEqual(self.mqttnode.config.mqtt_broker, "localhost")
+        self.assertEqual(self.mqttnode.config.user, "felix")
+        self.assertEqual(self.mqttnode.config.mqtt_port, 1883)
+        self.assertEqual(self.mqttnode.config.qos, 2)
+        self.assertEqual(self.mqttnode.config.ssl, False)
+        self.assertListEqual(sorted(["over", "test"]), sorted(self.mqttnode.config.topics))
+
+    def test_reads_config_correctly(self):
+        self.mqttnode = MQTTnode()
+        self.assertEqual(self.mqttnode.config.mqtt_broker, "fweiler.de")
+        self.assertEqual(self.mqttnode.config.user, "albatrozz")
+        self.assertEqual(self.mqttnode.config.mqtt_port, 8883)
+        self.assertEqual(self.mqttnode.config.qos, 2)
+        self.assertEqual(self.mqttnode.config.ssl, True)
+        self.assertListEqual(sorted(["pow", "imu"]), sorted(self.mqttnode.config.topics))
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/msb_mqtt/test_zmq.py
@@ -0,0 +1,33 @@
+from Publisher import Publisher
+from Subscriber import Subscriber
+import unittest
+import zmq
+import pickle
+import time
+
+
+class Test_Sender(unittest.TestCase):
+    def setUp(self):
+        port = 66665
+        self.topic = "imu".encode()
+        self.subscriber = Subscriber(port=port, topic=topic)
+        self.publisher = Publisher(port=port)
+        time.sleep(0.1)
+
+    def get_data(self):
+        while True:
+            (topic, data) = self.subscriber.receive()
+            return topic.decode()
+
+    def test_pub_sub_with_classes(self):
+        data = "Test pub sub with classes"
+
+        self.publisher.send("imu".encode(), data.encode())
+        time.sleep(0.1)
+        (topic_recv, message) = self.subscriber.receive()
+
+        self.assertEqual(message.decode(), data)
+
+
+if __name__ == "__main__":
+    unittest.main()
